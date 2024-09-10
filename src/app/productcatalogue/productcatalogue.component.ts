93ba349f2899154c170273f83b5aa76f89da317d
carlos.mato <carlos.mato@altia.es>
2024-09-10 16:25:10 +0200

    Prueba

---
+++ src/app/productcatalogue/productcatalogue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productcatalogue',
+  standalone: true,
+  imports: [],
+  templateUrl: './productcatalogue.component.html',
+  styleUrl: './productcatalogue.component.scss'
+})
+export class ProductcatalogueComponent {
+
+}
